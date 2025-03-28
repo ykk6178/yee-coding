92ef61f97d9efc0bfc8732a20ba1f20eed539d4c
louis <ykk6178@gmail.com>
2025-03-28 17:05:49 +0800

    router code

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
+import Nav from "./components/Navbar";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 
 root.render(
   <StrictMode>
     <BrowserRouter basename="/yee-coding">
+      <Nav />
       <App />
     </BrowserRouter>
   </StrictMode>
