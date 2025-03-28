032c4cbb05c5d31bd1c7f6240cb2463bbddf1f71
louis <ykk6178@gmail.com>
2025-03-28 18:14:19 +0800

    router change HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import App from "./App";
 import Nav from "./components/Navbar";
@@ -9,9 +9,9 @@ const root = ReactDOM.createRoot(document.getElementById("root")!);
 
 root.render(
   <StrictMode>
-    <BrowserRouter basename="/yee-coding">
+    <HashRouter>
       <Nav />
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
