43e7b5c91cc3ec4d75b2f1c4cbd3383407da3481
louis <ykk6178@gmail.com>
2025-03-28 13:29:17 +0800

    create router

---
+++ src/pages/About.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About: React.FC = () => {
+  return (
+    <div>
+      <h2>About Page</h2>
+      <p>Welcome to the About Page!</p>
+    </div>
+  );
+};
+
+export default About;
