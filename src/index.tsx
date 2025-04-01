57d857bd37bdee0cd04a38b98b699223850a0906
louis <ykk6178@gmail.com>
2025-04-01 16:39:50 +0800

    create redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import { StrictMode, lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/store";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Loading from "./components/Loading";
@@ -11,12 +13,14 @@ const root = ReactDOM.createRoot(document.getElementById("root")!);
 
 root.render(
   <StrictMode>
-    <HashRouter>
-      <Header />
-      <Suspense fallback={<Loading />}>
-        <App />
-      </Suspense>
-      <Footer />
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <Header />
+        <Suspense fallback={<Loading />}>
+          <App />
+        </Suspense>
+        <Footer />
+      </HashRouter>
+    </Provider>
   </StrictMode>
 );
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,17 @@
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "../../store/counterSlice"; // 导入 actions
+import type { RootState } from "../../store/store"; // 导入 RootState 类型
+
 const Home: React.FC = () => {
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
   return (
     <div>
+      <h1>Count: {count}</h1>
       <p>Welcome to the Home Page!</p>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
     </div>
   );
 };
